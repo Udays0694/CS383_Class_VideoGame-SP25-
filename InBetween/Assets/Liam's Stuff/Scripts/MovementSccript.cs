24f40fce23b14df5597cc6729f36d5862e9e9c2e
Liam Garner <liambgm@gmail.com>
2025-02-24 21:03:03 -0800

    Gravity and finalizing basic player movement

--- InBetween/Assets/Liam's Stuff/Scripts/MovementSccript.cs
+++ InBetween/Assets/Liam's Stuff/Scripts/MovementSccript.cs
@@ -17,23 +17,29 @@ public class PlayerMovement : MonoBehaviour
         movement.x = 0;
         movement.y = 0;
 
+
         if (Input.GetKey(KeyCode.W))
         {
             movement.y = 1;
+
+
         }
         if (Input.GetKey(KeyCode.S))
         {
             movement.y = -1;
+
         }
         if (Input.GetKey(KeyCode.A))
         {
             movement.x = -1;
+ 
         }
         if (Input.GetKey(KeyCode.D))
         {
             movement.x = 1;
         }
-        
+
+
         // Normalize movement to prevent faster diagonal movement
         movement = movement.normalized;
     }
