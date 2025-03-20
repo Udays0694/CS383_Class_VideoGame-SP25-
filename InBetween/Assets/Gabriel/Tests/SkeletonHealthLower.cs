bf954d6022c66a75e71f68376c43e7761f45e87a
topdog864 <gabrielroy1361@gmail.com>
2025-03-20 07:19:27 -0700

    tl5

---
+++ InBetween/Assets/Gabriel/Tests/SkeletonHealthLower.cs
@@ -0,0 +1,46 @@
+/*Hurts skeleton to see if they die*/
+
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+[TestFixture]
+public class SkeletonHealthLower
+{
+    private GameObject skeletonPrefab;
+    private SkeletonScript skeletonScript;
+
+    [OneTimeSetUp]
+    public void LoadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("RoomGenTest");
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        skeletonPrefab = Resources.Load<GameObject>("Assets/Gabriel/Prefabs/Skeleton");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (skeletonPrefab != null)
+        {
+            GameObject.Destroy(skeletonPrefab);
+        }
+    }
+
+    [UnityTest]
+    public IEnumerator SkeletonBoundaryStressTest()
+    {
+        GameObject skeleton = Object.Instantiate(skeletonPrefab, Vector3.zero, Quaternion.identity);
+        skeletonScript = skeleton.GetComponent<SkeletonScript>();
+        Assert.NotNull(skeletonScript, "Failed to get script");
+        skeletonScript.OnTakeDamage(100f);
+        yield return null;
+        Assert.IsNull(skeleton, "Didn't delete object");
+    }
+}
