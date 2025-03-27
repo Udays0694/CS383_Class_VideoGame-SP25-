aa707fe571c25043ff30fe1c95e47cb12b88673a
Tavin Yorgason <tazasor@gmail.com>
2025-03-26 23:31:34 -0700

    Improved boss attack, added animations

--- InBetween/Assets/Tavin_TL2+/Scripts/BossController.cs
+++ InBetween/Assets/Tavin_TL2+/Scripts/BossController.cs
@@ -11,8 +11,11 @@ public class BossController : MonoBehaviour
 
 	// Characteristics
 	private Slider healthBar; 
+    private Animator _animator;
 	private float attack1Timer = 0;
 	[SerializeField] private GameObject Bullet1;
+	private bool facingLeft = true;
+	private Vector2 mouthPos;
 
 	// Player
 	private GameObject Player;
@@ -29,20 +32,50 @@ public class BossController : MonoBehaviour
 		
 		// Get reference to player
 		Player = GameObject.FindGameObjectWithTag("Player");
+		
+		// Initiate animator
+		_animator = GetComponent<Animator>();
 	}
 
     // Update is called once per frame
     void Update()
     {
+		// Calculate mouth position and center of face
+		float xTransform = 1.1f;
+		float centerDiff = 1.54f;
+		if(!facingLeft)
+		{
+			xTransform = -xTransform;
+			centerDiff = -centerDiff;
+		}
+		
+		mouthPos = transform.position + new Vector3(xTransform, -2.3f, 0);
+
+		// Distance the boss must be transformed to make the flip look natural
+		float flipTransformDist = 3.08f;  
+		
 		// Calculate direction to player
-		playerDir.x = Player.transform.position.x - transform.position.x;
-		playerDir.y = Player.transform.position.y - transform.position.y;
+		playerDir = Player.transform.position - (transform.position + new Vector3(centerDiff, -3.5f, 0));
+		
+		// Face the player
+		if(facingLeft && playerDir.x > 0)//flipTransformDist)
+		{
+			transform.position += new Vector3(flipTransformDist, 0, 0);
+			transform.Rotate(0, 180, 0);
+			facingLeft = false;
+		}
+		else if(!facingLeft && playerDir.x < 0)//-flipTransformDist)
+		{
+			transform.position += new Vector3(-flipTransformDist, 0, 0);
+			transform.Rotate(0, 180, 0);
+			facingLeft = true;
+		}
 
 		// Attack player
 		chasePlayer();
         if(attack1Timer >= attack1Cooldown)
 		{
-			attack1();
+			_animator.Play("BossAttack");
 			attack1Timer = 0;
 		}
 
@@ -53,21 +86,40 @@ public class BossController : MonoBehaviour
 	public void attack1()
 	{
 		// Generate quaternion
-		Vector3 playerDir3d = new Vector3(playerDir.x, playerDir.y, 0);
-		Quaternion shootDir = Quaternion.LookRotation(Vector3.forward, playerDir3d);
-		Debug.Log("Shooting bullet");
-		Instantiate(Bullet1, transform.position, shootDir);
+//		Vector3 playerDir3d = new Vector3(playerDir.x, playerDir.y, 0);
+//		Quaternion shootDir = Quaternion.LookRotation(Vector3.forward, playerDir3d);
+		int xDir = 1;
+		if(facingLeft)
+		{
+			xDir = -1;
+		}
+		Quaternion shootDir = Quaternion.LookRotation(Vector3.forward, new Vector3(xDir, 0, 0));
+		
+		
+		Instantiate(Bullet1, mouthPos, shootDir);
+		
+		// Play animation
+		//_animator.Play("BossAttack");
 	}
 
 	// Move towards player
 	private void chasePlayer()
 	{
+		// Move away from player if its too close
+		Vector2 moveDir = playerDir;
+/*		if(playerDir.magnitude < 3.5f)
+		{
+			moveDir = -moveDir;
+		}
+*/		
 		// Calculate move distance
 		Vector2 move;
 		move.x = transform.position.x
-				 + playerDir.normalized.x * Time.deltaTime * speed;
+				 + moveDir.normalized.x * Time.deltaTime * speed;
 		move.y = transform.position.y
-				   	  + playerDir.normalized.y * Time.deltaTime * speed;
+				   	  + moveDir.normalized.y * Time.deltaTime * speed;
+		
+
 
 		// Apply movement
 		transform.position = move;
@@ -76,11 +128,20 @@ public class BossController : MonoBehaviour
 	// Take damage
 	public void takeDamage(float amount)
 	{
+		// Decrease health
 		health -= amount;
 		healthBar.value = health;
+		
+		// Die
 		if(health <= 0)
 		{
+			_animator.Play("BossDie");
 			Destroy(gameObject);
+		}
+		// Play damage animation
+		else
+		{
+			_animator.Play("BossHurt");
 		}
 	}
 }
--- InBetween/Assets/Tavin_TL2+/Scripts/BulletController.cs
+++ InBetween/Assets/Tavin_TL2+/Scripts/BulletController.cs
@@ -4,30 +4,69 @@ public class BulletController : MonoBehaviour
 {
 	// Characteristics
 	private float speed = 3f;
+	private Vector2 moveDir;
+	private float timer = 0;
+	private float deathTimer = 0;
+	
+	// Player
+	private GameObject Player;
+	private Vector2 playerDir;
 
     // Start is called once before the first execution of Update after the
 	// MonoBehaviour is created
     void Start()
     {
+    	// Movement direction
+ 		moveDir = transform.up;
+		
+        transform.Rotate(0, 0, -90);
         
+        // Get reference to player
+		Player = GameObject.FindGameObjectWithTag("Player");
+		
+		// Initiate animator
+//		_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+    	// Animate flame
+    	timer += Time.deltaTime;
+    	if(timer >= 0.08f)
+    	{
+    		timer = 0;
+    		transform.Rotate(180, 0, 0);
+    	}
+    	
+    	// Move
     	move();
+    	
+    	// Destroy if alive for too long
+    	deathTimer += Time.deltaTime;
+    	if(deathTimer >= 5)
+    	{
+    		Destroy(gameObject);
+    	}
     }
 
 	// Move the bullet in whatever direction its facing
 	private void move()
 	{
-		Vector2 moveDir = transform.up;
-		moveDir = moveDir.normalized * Time.deltaTime * speed;
-
-		moveDir.x += transform.position.x;
-		moveDir.y += transform.position.y;
+		const float LERP_AMT = 0.03f;
+		Vector3 playerDir = Player.transform.position - transform.position;
+		moveDir = new Vector2(Mathf.Lerp(moveDir.x, playerDir.x, LERP_AMT * (1 / playerDir.magnitude)),
+							  Mathf.Lerp(moveDir.y, playerDir.y, LERP_AMT * (1 / playerDir.magnitude)));
+		Vector3 move = moveDir.normalized * Time.deltaTime * speed;
 		
-		transform.position = moveDir;
+		// Generate quaternion 
+		transform.localRotation = Quaternion.LookRotation(Vector3.forward, moveDir);
+
+		// Apply the move
+		move += transform.position;
+//		move.y += transform.position.y;
+		
+		transform.position = move;
 	}
 
 	// Handle player collisions
